c52aa7305cbe3d92893861d44f6f515d74859bea
Vikram0817 <vikramchaudhary8700@gmail.com>
2024-03-12 11:52:48 +0530

    commit

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -10,7 +10,7 @@ function AppBar() {
 
   return (
     <div className='border-b flex justify-between px-10 py-4'>
-      <Link to="/">
+      <Link to="/blog">
         <div className='font-bold text-2xl cursor-pointer'>
             Medium
         </div>
